498590e3efc19aa704018971a5690e147c9c301d
Antony Tri Susanto Itran <63346091+maslakonetop@users.noreply.github.com>
2022-08-14 19:45:41 +0700

    Initial Upload

---
+++ resources/views/pages/bandara.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+@section('isikonten')
+<div class="section-title" data-aos="zoom-out">
+    <h2>Bandara</h2>
+    <p>Bandara Tunggul Wulung Cilacap</p>
+  </div>
+
+  <div class="row">
+    <div class="col-md-6 -mb-2">
+      <img src="/image/tunggulwulung.png" style="width: 800px" alt="" >
+    </div>
+    <div class="col-md-6">
+        
+    </div>    
+  </div>
+@endsection
